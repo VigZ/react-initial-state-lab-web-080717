502d89887b209589019c6bd02b6d7d2d3769e570
github username <github email address>
2017-10-10 12:20:53 -0400

    Done.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class ImageSlider extends React.Component{
+  constructor() {
+    super();
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  render(){
+    return(
+      <div>
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+
+      </div>
+    )
+  }
+}
+
+export default ImageSlider
 // Bomb Component Code Goes Here
